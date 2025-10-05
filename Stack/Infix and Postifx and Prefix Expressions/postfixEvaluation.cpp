0c894505b0cf06da19d5780ae2c15f0b46c785e3
Akshat-1407 <akshat.rastogi1407@gmail.com>
2025-10-05 13:03:37 +0530

    ...

--- Stack/Infix and Postifx and Prefix Expressions/postfixEvaluation.cpp
+++ Stack/Infix and Postifx and Prefix Expressions/postfixEvaluation.cpp
@@ -1,59 +1,7 @@
 #include <iostream>
-#include <vector>
+#include "../intStack.h"
 #include <cmath>
-#define STACKSIZE 20
 using namespace std;
-
-struct STACK {
-    int item[STACKSIZE];
-    int Top;
-};
-
-
-void Initialize(STACK &Stack) {
-    Stack.Top = -1;
-}
-
-
-bool isEmpty(STACK &Stack) {
-    if(Stack.Top == -1) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-
-void push(STACK &Stack, int x) {
-    if(Stack.Top == STACKSIZE-1) {        
-        cout<<"Stack Overflows"<<endl;
-        exit(1);
-    }
-    else {
-        Stack.Top++;
-        Stack.item[Stack.Top] = x;
-    }
-}
-
-
-int pop(STACK &Stack) {
-    int x;
-    if (isEmpty(Stack)) {
-        cout<<"Stack Underflows"<<endl;
-        exit(1);
-    }
-    else {      
-        x = Stack.item[Stack.Top];
-        Stack.Top--;
-    }
-    return x;
-}
-
-
-int StackTop(STACK &Stack) {
-    return Stack.item[Stack.Top];
-}
 
 
 int evaluate(int x, int y, char symbol) {
@@ -70,29 +18,29 @@ int evaluate(int x, int y, char symbol) {
 void postfixExpression(char postfix[]) {
     struct STACK operandStack;
     int i = 0;
-    Initialize(operandStack);
+    Initialize(&operandStack);
     while(postfix[i] != '\0') {
         char symbol = postfix[i];
         if (symbol >= '0' && symbol <= '9') {
-            push(operandStack, symbol - '0');
+            push(&operandStack, symbol - '0');
         }
         else {
-            int y = pop(operandStack);
-            int x = pop(operandStack);
+            int y = pop(&operandStack);
+            int x = pop(&operandStack);
 
             int value = evaluate(x, y, symbol);
 
-            push(operandStack ,value);
+            push(&operandStack ,value);
         }
         i++;
-        
     } 
-    cout<<pop(operandStack);   
+    cout<<pop(&operandStack);   
 }
 
 int main() {
 
     char expression[50];
+    cout << "Enter a Postfix Expression: ";
     cin>>expression;
 
     postfixExpression(expression);
--- Stack/Infix and Postifx and Prefix Expressions/prefixEvaluation.cpp
+++ Stack/Infix and Postifx and Prefix Expressions/prefixEvaluation.cpp
@@ -1,61 +1,9 @@
 #include <iostream>
+#include "../intStack.h"
 #include <vector>
-#include <cmath>
 #include <algorithm>
-#define STACKSIZE 20
+#include <cmath>
 using namespace std;
-
-struct STACK {
-    int item[STACKSIZE];
-    int Top;
-};
-
-
-void Initialize(STACK &Stack) {
-    Stack.Top = -1;
-}
-
-
-bool isEmpty(STACK &Stack) {
-    if(Stack.Top == -1) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-
-void push(STACK &Stack, int x) {
-    if(Stack.Top == STACKSIZE-1) {        
-        cout<<"Stack Overflows"<<endl;
-        exit(1);
-    }
-    else {
-        Stack.Top++;
-        Stack.item[Stack.Top] = x;
-    }
-}
-
-
-int pop(STACK &Stack) {
-    int x;
-    if (isEmpty(Stack)) {
-        cout<<"Stack Underflows"<<endl;
-        exit(1);
-    }
-    else {      
-        x = Stack.item[Stack.Top];
-        Stack.Top--;
-    }
-    return x;
-}
-
-
-int StackTop(STACK &Stack) {
-    return Stack.item[Stack.Top];
-}
-
 
 int evaluate(int x, int y, char symbol) {
     switch(symbol) {
@@ -71,24 +19,22 @@ int evaluate(int x, int y, char symbol) {
 void prefixExpression(vector<char> postfix) {
     struct STACK operandStack;
     int i = 0;
-    Initialize(operandStack);
-    while(postfix[i] != '\0') {
+    Initialize(&operandStack);
+    for (int i = 0; i < postfix.size(); i++) {
         char symbol = postfix[i];
         if (symbol >= '0' && symbol <= '9') {
-            push(operandStack, symbol - '0');
+            push(&operandStack, symbol - '0');
         }
         else {
-            int x = pop(operandStack);
-            int y = pop(operandStack);
+            int x = pop(&operandStack);
+            int y = pop(&operandStack);
 
             int value = evaluate(x, y, symbol);
 
-            push(operandStack ,value);
+            push(&operandStack ,value);
         }
-        i++;
-        
     } 
-    cout<<pop(operandStack);   
+    cout<<pop(&operandStack);   
 }
 
 int main() {
@@ -100,6 +46,3 @@ int main() {
     
     return 0;
 }
-
-
-
